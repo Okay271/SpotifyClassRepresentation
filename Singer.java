d34378460037790fcee4543c52536de29a4e706e
Okay271 <okayardah@gmail.com>
2024-04-23 11:14:32 +0300

    Update Singer Class
    Getter methods and toString method are  implemented.

--- Singer.java
+++ Singer.java
@@ -14,4 +14,33 @@ public class Singer {
         this.name = nameS;
         this.songs = new ArrayList<Song>();
     }
+     //get methods
+     public String getNameSinger()
+     {
+         return this.name;
+     }
+ 
+     public int getNumberOfSongs()
+     {
+         return this.songs.size();
+     }
+ 
+     //other methods
+     public void addSong(Song sng)
+     {
+         this.songs.add(sng);
+     }
+ 
+     //toString method
+     public String toString()
+     {
+         String info = "";
+         info += "-----" + this.getNameSinger() + "-----\n";
+         for (int i = 0; i < this.songs.size(); i++)
+         {
+             info += (i + 1) + ". " + this.songs.get(i).getSongName() + "\n";
+         }
+         info += "Total Number Of Songs: " + this.getNumberOfSongs(); 
+         return info;
+    }
 }
