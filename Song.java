dc1789f12da6fea7cc0840f7fb37613e297069a6
Okay271 <okayardah@gmail.com>
2024-04-25 10:43:30 +0300

    Update on Song Class
    Getter methods and toString method is created and implemented.

--- Song.java
+++ Song.java
@@ -17,6 +17,37 @@ public class Song
         this.type = typeS;
     }
 
+    //getter methods
+    public String getSongName()
+    {
+        return this.name;
+    }
+
+    public String getType()
+    {
+        return this.type;
+    }
+
+    public double getSongTime()
+    {
+        return this.time;
+    }
+
+    public String getSingerName()
+    {
+        return this.singerr.getNameSinger();
+    }
+
+    //toString method
+    public String toString()
+    {
+        String info = "";
+        info += "Name:   " + this.getSongName() + "\n";
+        info += "Singer: " + this.getSingerName() + "\n";
+        info += "Time:   " + this.getSongTime() + "\n";
+        info += "Type:   " + this.getType() + "\n";
+        return info;
+    }
 }
 
 
