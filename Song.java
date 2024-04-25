e6ab73913116ded7b50ddd88fe4671ca77980544
Okay271 <okayardah@gmail.com>
2024-04-25 10:42:28 +0300

    Create Song Class
    Song class is created. Instance variables and constructor is implemented

---
+++ Song.java
@@ -0,0 +1,22 @@
+package SPOTSPOT;
+
+public class Song
+{
+    // instance variables
+    private String name;
+    private Singer singerr;
+    private double time;
+    private String type;
+
+    // constructor
+    public Song(String nameS, Singer singerS, double timeS, String typeS)
+    {
+        this.name = nameS;
+        this.singerr = singerS;
+        this.time = timeS;
+        this.type = typeS;
+    }
+
+}
+
+
